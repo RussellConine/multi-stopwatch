652f4402a5373271edb09948b455233100d5f1d5
Russell Conine <russ.conine@gmail.com>
2019-02-05 12:01:25 -0800

    Add functionality to save Station, ID, and time to csv file

--- multiStopwatch.py
+++ multiStopwatch.py
@@ -2,12 +2,20 @@
 
 from tkinter import *
 import datetime
+import csv
 
 class GUI:
 	def __init__(self, mainWin):
+		""" Create GUI labels, buttons, and boxes
+		"""
 		frame = Frame(mainWin)
 		frame.pack()
 		mainWin.title('Timer')
+
+		### Init variables ###
+		self.curTime = '0:00:00'
+		self.counting = False
+		self.startTime = datetime.timedelta(0)
 
 		### Create top row of labels ###
 		self.label1 = Label(frame, text = 'Start')
@@ -38,48 +46,64 @@ class GUI:
 		self.E1 = Entry(frame)
 		self.E1.grid(row=1,column=1)
 
-		self.E1 = Entry(frame)
-		self.E1.grid(row=1,column=2)
+		self.E2 = Entry(frame)
+		self.E2.grid(row=1,column=2)
 
-		self.L1 = Label(frame, text = 'timeCount')
+		self.L1 = Label(frame, text = self.curTime)
 		self.L1.grid(row=1,column=3)
 		
-		self.B1 = Button(frame, text = 'Pause', command=self.start)
-		self.B1.grid(row=1,column=4)
+		self.B2 = Button(frame, text = 'Pause', command=self.pause)
+		self.B2.grid(row=1,column=4)
 
-		self.B1 = Button(frame,text = 'Stop', command=self.start)
-		self.B1.grid(row=1,column=5)
+		self.B3 = Button(frame,text = 'Stop', command=self.stop)
+		self.B3.grid(row=1,column=5)
 
-		self.B1 = Button(frame, text = 'Save', command=self.start)
-		self.B1.grid(row=1,column=6)
+		self.B4 = Button(frame, text = 'Save', command=self.save)
+		self.B4.grid(row=1,column=6)
 
 	def start(self):
-		now = datetime.datetime.now()
-	
-	def timeCount(self):
-		pass
+		""" Start stopwatch. Called by pressing Start button.
+		"""
+		self.now = datetime.datetime.now()
+		self.counting = True	
 
 	def pause(self):
-		pass
+		""" Pause stopwatch. Saves time elapsed as timedelta variable self.startTime
+		"""
+		self.startTime = datetime.datetime.now()-self.now
+		print(self.startTime)
 
 	def stop(self):
-		pass
+		""" Stop stopwatch. Saves time elapsed. Sets self.startTime=0.
+		"""
+		self.elapsed = datetime.datetime.now()-self.now+self.startTime
+		self.startTime = datetime.timedelta(0)
+		self.counting = False
 
 	def save(self):
-		pass		
-			
+		""" Appends Job Station, Job ID, and Time as CSV file. If file doesn't already 
+			exist, creates file and headers.
+		"""
+		try:
+			f = open('outfile.csv', 'r')
+			f.close()
+		except:
+			f = open('outfile.csv', 'w', newline = '')
+			header = ['Job Station', 'Job ID', 'Time']
+			csvWriter = csv.writer(f)
+			csvWriter.writerow(header)
+			f.close()
 
+		f = open('outfile.csv', 'a', newline = '')
+		csvWriter = csv.writer(f)
+		outRow = [self.E1.get(), self.E2.get(), str(self.elapsed)[:7]]
+		csvWriter.writerow(outRow)
+		f.close()
 
 		
-
-        # self.label = Label(mainWin, text='This is our first GUI!')
-        # self.label.pack()
-
-        # self.greet_button = Button(mainWin, text='Greet', command=self.greet)
-        # self.greet_button.pack()
-
-        # self.close_button = Button(mainWin, text='Close', command=mainWin.quit)
-        # self.close_button.pack()
+	# def updateWindow(self):
+	# 	self.L1.config(text = self.curTime)
+	
 
 root = Tk()
 window = GUI(root)
