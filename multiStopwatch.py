bf41366291e9b3a002a19b88fe8958cbf9ff8821
Russell Conine <russ.conine@gmail.com>
2019-02-06 10:10:37 -0800

    Add comments for button methods

--- multiStopwatch.py
+++ multiStopwatch.py
@@ -13,9 +13,9 @@ class GUI:
 		mainWin.title('Timer')
 
 		### Init variables ###
-		self.curTime = '0:00:00'
-		self.counting = False
-		self.startTime = datetime.timedelta(0)
+		self.curTime = StringVar()
+		self.curTime.set('0:00:00')
+		self.elapsed = datetime.timedelta(0)
 
 		### Create top row of labels ###
 		self.label1 = Label(frame, text = 'Start')
@@ -49,36 +49,51 @@ class GUI:
 		self.E2 = Entry(frame)
 		self.E2.grid(row=1,column=2)
 
-		self.L1 = Label(frame, text = self.curTime)
+		self.L1 = Label(frame, textvariable = self.curTime)
 		self.L1.grid(row=1,column=3)
 		
-		self.B2 = Button(frame, text = 'Pause', command=self.pause)
+		self.B2 = Button(frame, text = 'Pause', state = DISABLED, command=self.pause)
 		self.B2.grid(row=1,column=4)
 
-		self.B3 = Button(frame,text = 'Stop', command=self.stop)
+		self.B3 = Button(frame,text = 'Stop', state = DISABLED, command=self.stop)
 		self.B3.grid(row=1,column=5)
 
-		self.B4 = Button(frame, text = 'Save', command=self.save)
+		self.B4 = Button(frame, text = 'Save', state = DISABLED, command=self.save)
 		self.B4.grid(row=1,column=6)
 
 	def start(self):
 		""" Start stopwatch. Called by pressing Start button.
+			Stopwatch can be paused or stopped after starting.
 		"""
+		self.curTime.set('-:--:--')
 		self.now = datetime.datetime.now()
-		self.counting = True	
+		self.B1.config(state = DISABLED)
+		self.B2.config(state = NORMAL)
+		self.B3.config(state = NORMAL)
 
 	def pause(self):
-		""" Pause stopwatch. Saves time elapsed as timedelta variable self.startTime
+		""" Pause stopwatch. Saves time elapsed as timedelta variable self.elapsed.
+			Only save button is enabled after pausing stopwatch.
 		"""
-		self.startTime = datetime.datetime.now()-self.now
-		print(self.startTime)
+		self.elapsed = datetime.datetime.now()-self.now + self.elapsed
+		self.curTime.set(str(self.elapsed)[:7])
+		self.B1.config(state = NORMAL)
+		self.B2.config(state = DISABLED)
+		self.B3.config(state = DISABLED)
+		self.B4.config(state = DISABLED)
+
 
 	def stop(self):
 		""" Stop stopwatch. Saves time elapsed. Sets self.startTime=0.
+			Time can be 
 		"""
-		self.elapsed = datetime.datetime.now()-self.now+self.startTime
+		self.elapsed = datetime.datetime.now()-self.now + self.elapsed
+		self.curTime.set(str(self.elapsed)[:7])
 		self.startTime = datetime.timedelta(0)
-		self.counting = False
+		self.B1.config(state = DISABLED)
+		self.B2.config(state = DISABLED)
+		self.B3.config(text = 'Reset', command= self.reset)
+		self.B4.config(state = NORMAL)
 
 	def save(self):
 		""" Appends Job Station, Job ID, and Time as CSV file. If file doesn't already 
@@ -99,7 +114,19 @@ class GUI:
 		outRow = [self.E1.get(), self.E2.get(), str(self.elapsed)[:7]]
 		csvWriter.writerow(outRow)
 		f.close()
+		self.reset()
 
+	def reset(self):
+		self.curTime.set('0:00:00')
+		self.elapsed = datetime.timedelta(0)
+		self.E1.delete(0, END)
+		self.E2.delete(0, END)
+		self.initial = True
+		self.B1.config(state = NORMAL)
+		self.B3.config(text = 'Stop', state = DISABLED, command = self.stop)
+		self.B4.config(state = DISABLED)
+
+	
 		
 	# def updateWindow(self):
 	# 	self.L1.config(text = self.curTime)
